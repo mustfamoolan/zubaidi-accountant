4eeed4bd601d8bbe9d318b621bce3937b49825e5
mustfamoolan <almoq3m@gmail.com>
2025-10-26 02:16:50 +0300

    Add multiple admin and visitor accounts to AdminSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -13,12 +13,58 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
+        // إضافة المدير الرئيسي
         User::create([
             'name' => 'admin',
             'email' => 'admin@alzubaidi.com',
             'email_verified_at' => now(),
             'password' => Hash::make('12345678'),
             'role' => 'admin',
+        ]);
+
+        // إضافة مدير ثاني
+        User::create([
+            'name' => 'مدير واحد',
+            'email' => 'manager1@alzubaidi.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'role' => 'admin',
+        ]);
+
+        // إضافة مدير ثالث
+        User::create([
+            'name' => 'مدير اثنين',
+            'email' => 'manager2@alzubaidi.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('12345678'),
+            'role' => 'admin',
+        ]);
+
+        // إضافة زائر أول
+        User::create([
+            'name' => 'زائر واحد',
+            'email' => 'visitor1@alzubaidi.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('180370'),
+            'role' => 'visitor',
+        ]);
+
+        // إضافة زائر ثاني
+        User::create([
+            'name' => 'زائر اثنين',
+            'email' => 'visitor2@alzubaidi.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('180370'),
+            'role' => 'visitor',
+        ]);
+
+        // إضافة زائر ثالث
+        User::create([
+            'name' => 'زائر ثلاثة',
+            'email' => 'visitor3@alzubaidi.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('180370'),
+            'role' => 'visitor',
         ]);
     }
 }
